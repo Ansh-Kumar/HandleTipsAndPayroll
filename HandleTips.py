cb793ff28eae027bcacf056dd5bba02423901f60
Ansh Kumar Khalasi <ansh.khalasi@gmail.com>
2020-07-17 09:43:26 -0700

    shiftFinal() is complete
    shiftFinal() is the final version of the First Function.
    
    The idea is to create a new file known as ShiftFinal.py which will have all of the presented code. And the new version of HandleTips.py will call not only that file but the rest of them.

--- HandleTips.py
+++ HandleTips.py
@@ -4,6 +4,7 @@
 import os
 import csv
 import datetime
+import time
 
 # "/Users/anshkumar/Downloads/shifts-export_2020-07-01_2020-07-15.csv"
 
@@ -83,6 +84,8 @@ def convertTimeElapsed(seconds):
     hour = int(seconds/3600)
     minute = int((seconds-(3600*hour))/60)
     second = int((seconds - (3600*hour)-(minute*60)))
+    timeRtrn = str(hour) + ":" + str(minute)
+    return timeRtrn
     print(hour, minute, second)
 
 def openShift():
@@ -90,20 +93,97 @@ def openShift():
     with open(resources[0], mode = 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ',')
         lineCount = 0
+        data = []
+        dataF = []
         for row in csv_reader:
             if lineCount == 0:
                 lineCount += 1
                 continue
             else:
                 if row[1] != "":
-                    print("Name: ", row[1])
-                    print("Date: ", row[4])
-                    print("Start Time: ", row[5])
-                    print("End Time: ", row[7])
+                    data.append(row[1])
+                    data.append(row[4])
+                    data.append(row[5])
+                    data.append(row[7])
                     startN = row[5]
                     endN = row[7]
                     start = convertToSec(startN)
                     end = convertToSec(endN)
                     timeElapsed = end-start
                     timeElapsed = convertTimeElapsed(timeElapsed)
-                    print(timeElapsed)
+                    data.append(timeElapsed)
+                    print(data)
+            print(type(data))
+            dataF.append(data)
+            data = []
+        dataF.pop()
+    return dataF
+
+def findTotalTimePerPerson():
+    data = openShift()
+    print(data)
+    anshHours = 0
+    lynnHours = 0
+    nicoHours = 0
+    tamHours = 0
+    juanHours = 0
+    for row in data:
+        if row[0] == "Ansh":
+            hoursS = row[4]
+            hours = int(hoursS[0])
+            minuteS = hoursS[2] + hoursS[3]
+            minute = int(minuteS)
+            print(hours)
+            print(minute)
+            anshHours += hours + (minute/60)
+        elif row[0] == "Lynn":
+            hoursS = row[4]
+            hours = int(hoursS[0])
+            minuteS = hoursS[2] + hoursS[3]
+            minute = int(minuteS)
+            print(hours)
+            print(minute)
+            lynnHours += hours + (minute/60)
+        elif row[0] == "Tam":
+            hoursS = row[4]
+            hours = int(hoursS[0])
+            minuteS = hoursS[2] + hoursS[3]
+            minute = int(minuteS)
+            print(hours)
+            print(minute)
+            tamHours += hours + (minute/60)
+        elif row[0] == "Nico":
+            hoursS = row[4]
+            hours = int(hoursS[0])
+            minuteS = hoursS[2] + hoursS[3]
+            minute = int(minuteS)
+            print(hours)
+            print(minute)
+            nicoHours += hours + (minute/60)
+        else:
+            hoursS = row[4]
+            hours = int(hoursS[0])
+            minuteS = hoursS[2] + hoursS[3]
+            minute = int(minuteS)
+            print(hours)
+            print(minute)
+            juanHours += hours + (minute/60)
+    lstHours = []
+    lstHours.append(["Ansh", round(anshHours, 2)])
+    lstHours.append(["Lynn", round(lynnHours, 2)])
+    lstHours.append(["Tam", round(tamHours, 2)])
+    lstHours.append(["Nico", round(nicoHours, 2)])
+    lstHours.append(["Juan", round(juanHours, 2)])
+    return lstHours
+
+def shiftFinal():
+    data = findTotalTimePerPerson()
+    date = datetime.date.today().strftime("%m%d%y")
+    newFileName = "ShiftReports" + date + ".csv"
+    with open(newFileName, mode = "w", newline='') as csv_file:
+        columnNames = ["Name", "Total Hours"]
+        writer = csv.writer(csv_file)
+
+        writer.writerow(columnNames)
+        writer.writerows(data)
+    
